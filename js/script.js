dde719e84d48821837840f9220dad4f2bea2ec6b
MDanielMacedo <mdanielmacedo78@gmail.com>
2022-03-29 22:31:48 -0300

    reformulando

---
+++ js/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+  $('.slider').slick({
+    dots: true,
+    infinite: true,
+    speed: 1000,
+    fade: true,
+    cssEase: 'linear',
+    slidesToShow: 1,
+    adaptiveHeight: true,
+    autoplay: true,
+  });
+
+});
